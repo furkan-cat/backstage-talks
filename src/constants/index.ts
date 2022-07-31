7ccc84f52bed770496b8504b97f1417f4ae0f7dc
Furkan Çat <furkan.cat@bitci.com>
2022-07-31 21:28:54 +0300

    fixed sass installation problem

---
+++ src/constants/index.ts
@@ -0,0 +1,31 @@
+interface IMock {
+  id: string;
+  image: string;
+}
+
+export const mock: IMock[] = [
+  {
+    id: "1",
+    image: "../../public/images/1.png",
+  },
+  {
+    id: "2",
+    image: "../../public/images/2.png",
+  },
+  {
+    id: "3",
+    image: "../../public/images/3.png",
+  },
+  {
+    id: "4",
+    image: "../../public/images/4.png",
+  },
+  {
+    id: "5",
+    image: "../../public/images/5.png",
+  },
+  {
+    id: "6",
+    image: "../../public/images/6.png",
+  },
+];
